5dd603acb65226e206bceafcd67e7dd6c3a7fafb
hung977 <53803587+hung977@users.noreply.github.com>
2020-07-27 09:16:35 +0700

    Update Product.swift

--- FinalApp/Model/Product.swift
+++ FinalApp/Model/Product.swift
@@ -1,1 +1,20 @@
+import Foundation
 
+public struct ProductResponse: Codable {
+
+        public var items : [Product]
+        public var totalItems : Int
+        public var totalPages : Int
+        public var pageSize : Int
+        public var pageIndex : Int
+        
+}
+public struct Product: Codable {
+
+    public var name: String
+    public var price: Double
+    public var amount: Int
+    public var image: String
+    public var id: String
+        
+}
