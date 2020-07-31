22656675443660ba1537f78cc5eb9123c4b54da5
hung977 <hidden@gmail.com>
2020-07-31 10:01:58 +0700

    Add animation on LoginViewController

--- FinalApp/Login/LoginViewController.swift
+++ FinalApp/Login/LoginViewController.swift
@@ -17,7 +17,14 @@ class LoginViewController: UIViewController {
     //MARK: - IBoutlet
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var imageview: UIImageView!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var leadingContraintImg: NSLayoutConstraint!
+    @IBOutlet weak var usernameLeading: NSLayoutConstraint!
+    @IBOutlet weak var userTFLeading: NSLayoutConstraint!
+    @IBOutlet weak var passwdLeading: NSLayoutConstraint!
+    @IBOutlet weak var passTFLeading: NSLayoutConstraint!
+    @IBOutlet weak var loginLeading: NSLayoutConstraint!
     
     //MARK: - Life cycel
     override func viewDidLoad() {
@@ -27,6 +34,37 @@ class LoginViewController: UIViewController {
         passwordTextField.delegate = self
         usernameTextField.delegate = self
         setUI()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        leadingContraintImg.constant -= view.bounds.height
+        usernameLeading.constant -= view.bounds.width
+        userTFLeading.constant += view.bounds.width
+        passwdLeading.constant -= view.bounds.width
+        passTFLeading.constant += view.bounds.width
+        loginLeading.constant -= view.bounds.width
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0,
+                       delay: 0.0,
+                       options: [],
+                       animations: { [weak self] in
+                        self?.leadingContraintImg.constant = 20
+                        self?.usernameLeading.constant = 20
+                        self?.userTFLeading.constant = 20
+                        self?.passwdLeading.constant = 20
+                        self?.passTFLeading.constant = 20
+                        self?.loginLeading.constant = 20
+                        self?.view.layoutIfNeeded()
+          }, completion: nil)
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.imageview.transform = CGAffineTransform.identity.scaledBy(x: 1.5, y: 1.5)
+            self.loginBtn.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+         }) { (finished) in
+            UIView.animate(withDuration: 1.0, animations: {
+              self.imageview.transform = CGAffineTransform.identity
+              self.loginBtn.transform = CGAffineTransform.identity
+           })
+        }
     }
     //MARK: - IBAction
     
